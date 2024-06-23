3512e2efea0c5944aad13b464c0a4e94f5405871
Sifatul Rabbi <mdsifatulislam.rabbi@gmail.com>
2024-06-23 12:21:06 +0600

    practice with md5

--- solve2015/day4.go
+++ solve2015/day4.go
@@ -1,1 +1,17 @@
 package solve2015
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
+	"log"
+)
+
+func SolveDay4Part1(secret string) {
+	hasher := md5.New()
+	if _, err := hasher.Write([]byte(secret)); err != nil {
+		log.Fatalln(err)
+	}
+	md5Sig := hex.EncodeToString(hasher.Sum(nil))
+	fmt.Println(md5Sig)
+}
---
+++ solve2015/day4_test.go
@@ -0,0 +1,7 @@
+package solve2015
+
+import "testing"
+
+func TestDay4Part1Pre(t *testing.T) {
+	SolveDay4Part1("abcdef609043")
+}
