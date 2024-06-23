5b24b14638d21c4719e88254e67dd731404e2d06
Sifatul Rabbi <mdsifatulislam.rabbi@gmail.com>
2024-06-23 11:04:51 +0600

    solve: 2015 day3 part2

--- solve2015/day3.go
+++ solve2015/day3.go
@@ -9,7 +9,7 @@ func genMatrix(l int) [][]int {
 	return mtx
 }
 
-func SolveDay3(moves string) int {
+func SolveDay3Part1(moves string) int {
 	var (
 		l         = len(moves)
 		mtx       = genMatrix(l)
@@ -29,6 +29,47 @@ func SolveDay3(moves string) int {
 		x = x + m[0]
 		y = y + m[1]
 		mtx[y][x] += 1
+	}
+
+	housesReceived := 0
+	for _, vy := range mtx {
+		for _, vx := range vy {
+			if vx > 0 {
+				housesReceived++
+			}
+		}
+	}
+	return housesReceived
+}
+
+func SolveDay3Part2(moves string) int {
+	var (
+		l         = len(moves)
+		mtx       = genMatrix(l)
+		sy        = l - 1
+		sx        = l - 1
+		ry        = l - 1
+		rx        = l - 1
+		moveTable = map[string][2]int{
+			"^": {0, -1},
+			"v": {0, 1},
+			">": {1, 0},
+			"<": {-1, 0},
+		}
+	)
+
+	mtx[sy][sx] += 1
+	for i := 0; i < l; i++ {
+		m := moveTable[string(moves[i])]
+		if i%2 != 0 {
+			// santa
+			sx, sy = sx+m[0], sy+m[1]
+			mtx[sy][sx] += 1
+		} else {
+			// robo-santa
+			rx, ry = rx+m[0], ry+m[1]
+			mtx[ry][rx] += 1
+		}
 	}
 
 	housesReceived := 0
