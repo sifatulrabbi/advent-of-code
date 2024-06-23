c9367dc066420bb08d9961a29b72801c95ab4a2d
Sifatul Rabbi <mdsifatulislam.rabbi@gmail.com>
2024-06-23 15:50:17 +0600

    solve: 2015 day 5 part 2

--- solve2015/day5.go
+++ solve2015/day5.go
@@ -20,14 +20,14 @@ func SolveDay5Part1(input string) int {
 	lines := strings.Split(input, "\n")
 	totalNice := 0
 	for _, l := range lines {
-		if isNiceString(l) {
+		if isNiceStringP1(l) {
 			totalNice++
 		}
 	}
 	return totalNice
 }
 
-func isNiceString(str string) bool {
+func isNiceStringP1(str string) bool {
 	arr := strings.Split(str, "")
 	hasDouble := false
 	vowels := 0
@@ -47,6 +47,57 @@ func isNiceString(str string) bool {
 		prev = s
 	}
 	if hasDouble && vowels >= 3 {
+		return true
+	}
+	return false
+}
+
+func SolveDay5Part2(input string) []string {
+	lines := strings.Split(input, "\n")
+	totalNice := []string{}
+	for _, l := range lines {
+		if isNiceStringP2(l) {
+			totalNice = append(totalNice, l)
+		}
+	}
+	return totalNice
+}
+
+func isNiceStringP2(str string) bool {
+	combo2 := map[string]int{}
+	combo3 := 0
+	arr := strings.Split(str, "")
+
+	for i := 0; i < len(arr); i++ {
+		if k := i + 2; k < len(arr) && arr[i] == arr[k] {
+			combo3++
+		}
+
+		if j := i + 1; j < len(arr) {
+			combo := arr[i] + arr[j]
+			valid := true
+			if arr[i] == arr[j] &&
+				!(j+1 < len(arr) && arr[j] == arr[j+1]) &&
+				i-1 >= 0 && arr[i] == arr[i-1] {
+				valid = false
+			}
+			if valid {
+				if _, ok := combo2[combo]; ok {
+					combo2[combo]++
+				} else {
+					combo2[combo] = 1
+				}
+			}
+		}
+	}
+
+	combo2count := 0
+	for _, v := range combo2 {
+		if v >= 2 {
+			combo2count += v
+		}
+	}
+	if combo3 > 0 && combo2count >= 2 {
 		return true
 	}
 	return false
