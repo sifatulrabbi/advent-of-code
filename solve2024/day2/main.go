035f580626cc63c87da9f0f2074b47921369898b
Sifatul Rabbi <mdsifatulislam.rabbi@gmail.com>
2024-12-02 12:54:44 +0600

    solving day2, 2024

--- solve2024/day2/main.go
+++ solve2024/day2/main.go
@@ -17,23 +17,35 @@ func main() {
 	input := string(f)
 	reports := reportsToIntArr(strings.Split(input, "\n"))
 	solvePart1(reports)
+	solvePart2(reports)
 }
 
 func solvePart1(reports [][]int) {
 	safe := 0
 	for _, r := range reports {
 		ok := true
-		for i := 1; i < len(r)-1; i++ {
-			if !isDiffWithinLimit(r[i-1], r[i]) || !isDiffWithinLimit(r[i], r[i+1]) {
+		asc := 0
+		for i := 1; i < len(r); i++ {
+			if !isDiffWithinLimit(r[i-1], r[i]) {
 				ok = false
 				break
 			}
-			if (r[i-1] < r[i] && !(r[i] < r[i+1])) ||
-				(r[i-1] > r[i] && !(r[i] > r[i+1])) {
+
+			isIncreasing := r[i-1] < r[i]
+			if asc == 0 {
+				if isIncreasing {
+					asc = 1
+				} else {
+					asc = -1
+				}
+				continue
+			}
+			if (isIncreasing && asc != 1) || (!isIncreasing && asc != -1) {
 				ok = false
 				break
 			}
 		}
+
 		if ok {
 			safe++
 		}
@@ -41,17 +53,66 @@ func solvePart1(reports [][]int) {
 	fmt.Println("Part 1:", safe)
 }
 
+func solvePart2(reports [][]int) {
+	safe := 0
+	for _, r := range reports {
+		skip := -1
+		for i := 0; i < len(r); i++ {
+			ok := isValidReport(r, skip)
+			if ok {
+				safe++
+				break
+			} else {
+				skip = i
+			}
+		}
+	}
+	fmt.Println("Part 2:", safe)
+}
+
+func isValidReport(r []int, skip int) bool {
+	ok, asc := true, 0
+	for i := 1; i < len(r)-1; i++ {
+		next := i + 1
+		if skip > -1 && skip == next {
+			next++
+		}
+		if !isDiffWithinLimit(r[i], r[next]) {
+			ok = false
+			break
+		}
+
+		isIncreasing := r[i] < r[next]
+		if asc == 0 {
+			if isIncreasing {
+				asc = 1
+			} else {
+				asc = -1
+			}
+			continue
+		}
+		if (isIncreasing && asc != 1) || (!isIncreasing && asc != -1) {
+			ok = false
+			break
+		}
+	}
+	return ok
+}
+
 func isDiffWithinLimit(v1, v2 int) bool {
 	diff := v1 - v2
 	if diff < 0 {
 		diff = -diff
 	}
-	return diff < 4
+	return diff > 0 && diff < 4
 }
 
 func reportsToIntArr(reports []string) [][]int {
 	intArr := [][]int{}
 	for _, report := range reports {
+		if report == "" {
+			continue
+		}
 		arrStr := strings.Split(report, " ")
 		arr := []int{}
 		for _, v := range arrStr {
