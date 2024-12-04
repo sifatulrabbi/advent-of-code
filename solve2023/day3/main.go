a406c5e3326f585a54e802120ae0cc67c9edb101
Sifatul Rabbi <mdsifatulislam.rabbi@gmail.com>
2024-12-04 09:07:30 +0600

    solving day3 part1 2023

--- solve2023/day3/main.go
+++ solve2023/day3/main.go
@@ -1,9 +1,32 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
-	"strconv"
+	"slices"
+)
+
+type Token struct {
+	Value string
+	Type  string
+}
+
+type Expression struct {
+	Tokens []Token
+}
+
+type Program struct {
+	input       string
+	Tokens      []Token
+	Expressions []Expression
+	currIdx     int
+	peekIdx     int
+}
+
+const (
+	NumberType = "NUMBER"
+	SymbolType = "SYMBOL"
 )
 
 func main() {
@@ -17,30 +40,76 @@ func main() {
 }
 
 func part1(input string) {
+	p := Program{input: input, currIdx: 0, peekIdx: 1}
+
+	p.ScanTokens()
+	p.Print()
+
+	p.Parse()
 }
 
 func part2(input string) {
 }
 
-func parseInput(input string) []string {
-	arr := []string{}
-	for i := 0; i < len(input); i++ {
-		c := string(input[i])
-		if c == "." {
-			continue
+func (p *Program) ScanTokens() {
+	for p.currIdx < len(p.input) {
+		char := string(p.input[p.currIdx])
+		if isNumber(char) {
+			p.scanNumber()
+		} else if isSymbol(char) {
+			p.scanSymbol()
 		}
-		n := ""
-		for j := 0; j < len(c); j++ {
-			if isNumber(string(c[j])) {
-				n += string(c[j])
-			}
-		}
-		arr = append(arr, n)
+		p.currIdx++
+		p.peekIdx++
 	}
-	return arr
+}
+
+// TODO:
+func (p *Program) Parse() {
+	for i := 0; i < len(p.Tokens); i++ {
+		tok := p.Tokens[i]
+		if tok.Type == NumberType {
+			// p.Expressions = append(p.Expressions, Expression{Tokens: []Token{tok}})
+		}
+	}
+}
+
+func (p *Program) peek() string {
+	if p.peekIdx < len(p.input) {
+		return string(p.input[p.peekIdx])
+	}
+	return ""
+}
+
+func (p *Program) scanNumber() {
+	for isNumber(p.peek()) {
+		p.peekIdx++
+	}
+	tok := Token{Value: p.input[p.currIdx:p.peekIdx], Type: NumberType}
+	p.Tokens = append(p.Tokens, tok)
+	p.currIdx = p.peekIdx
+}
+
+func (p *Program) scanSymbol() {
+	for isSymbol(p.peek()) {
+		p.peekIdx++
+	}
+	tok := Token{Value: p.input[p.currIdx:p.peekIdx], Type: SymbolType}
+	p.Tokens = append(p.Tokens, tok)
+	p.currIdx = p.peekIdx
+}
+
+func (p Program) Print() {
+	for _, tok := range p.Tokens {
+		fmt.Printf("%s[%s] ", tok.Type, tok.Value)
+	}
+	fmt.Println()
 }
 
 func isNumber(c string) bool {
-	_, err := strconv.Atoi(c)
-	return err == nil
+	return slices.Contains([]string{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}, c)
+}
+
+func isSymbol(c string) bool {
+	return slices.Contains([]string{"+", "-", "*", "/", "@", "#", "$", "%", "^", "&", "=", "~", "`", "?", "!", "<", ">", ";", ":", "[", "]", "{", "}", "|", "\\"}, c)
 }
